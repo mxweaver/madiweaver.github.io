519fa1d376d8f0954cd071c34bf92aae3fdbc831
Maya Vera <github@mayavera.me>
2017-08-02 21:11:52 -0400

    Moved reset to index.scss

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,13 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.scss$/, exclude: /node_modules/, use: [{
+        loader: "style-loader" // creates style nodes from JS strings
+      }, {
+        loader: "css-loader" // translates CSS into CommonJS
+      }, {
+        loader: "sass-loader" // compiles Sass to CSS
+      }]}
     ],
   },
 
