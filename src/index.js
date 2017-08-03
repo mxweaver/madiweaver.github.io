c3d27fa6572ec8a5afc4092c603b5af723ae5dc0
Maya Vera <github@mayavera.me>
2017-08-02 21:20:16 -0400

    Added roboto font

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App'
-import './styles/reset.scss'
+import './index.scss'
 
 ReactDOM.render(
   <BrowserRouter>
