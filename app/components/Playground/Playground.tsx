20041ee7cdaa3d992e33dfd8ad6be27612288015
Maya Vera <maya@mayavera.me>
2019-03-02 13:12:56 -0500

    altered playground to create circles on touch

--- app/components/Playground/Playground.tsx
+++ app/components/Playground/Playground.tsx
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
+import produce from 'immer'
+import classnames from 'classnames'
 import animate, { WrappedProps } from '../../hoc/animate'
 import c from './Playground.scss'
 
-interface State {
+interface Circle {
+  radius: number;
   x: number;
   y: number;
 }
 
-class Playground extends Component<WrappedProps, State> {
+interface Props extends WrappedProps {
+  className?: string;
+}
+
+interface State {
+  newCircle?: Circle;
+  circles: Circle[];
+}
+
+class Playground extends Component<Props, State> {
   public state: State = {
-    x: 0,
-    y: 0
+    circles: []
   }
 
   public componentDidMount() {
@@ -19,17 +30,59 @@ class Playground extends Component<WrappedProps, State> {
 
   public componentDidUpdate(prevProps: WrappedProps) {
     if (this.props.step !== prevProps.step) {
-      this.setState({
-        x: this.state.x + 1,
-        y: this.state.y + 1
-      })
+      if (this.state.newCircle)
+        this.setState(produce((draft: State) => {
+          draft.newCircle.radius++
+        }))
+    }
+  }
+
+  public handleMouseDown = (event: React.MouseEvent<SVGSVGElement>) => {
+    this.setState({
+      newCircle: {
+        x: event.clientX,
+        y: event.clientY,
+        radius: 10
+      }
+    })
+  }
+
+  public handleMouseUp = () => {
+    if (this.state.newCircle) {
+      this.setState(produce((draft: State) => {
+        draft.circles.push(draft.newCircle)
+        draft.newCircle = undefined
+      }))
+    }
+  }
+
+  public handleMouseMove = (event: React.MouseEvent<SVGSVGElement>) => {
+    const { clientX, clientY } = event
+
+    if (this.state.newCircle) {
+      this.setState(produce((draft: State) => {
+        draft.newCircle.x = clientX
+        draft.newCircle.y = clientY
+      }))
     }
   }
 
   public render() {
+    const { newCircle, circles } = this.state
+
     return (
-      <svg className={c.display}>
-        <rect x={this.state.x} y={this.state.y} width={100} height={100} fill='red' />
+      <svg
+        className={classnames(this.props.className, c.display)}
+        onMouseDown={this.handleMouseDown}
+        onMouseUp={this.handleMouseUp}
+        onMouseMove={this.handleMouseMove}
+      >
+        {circles.map((c, i) => <circle key={i} r={c.radius} cx={c.x} cy={c.y} />)}
+        {newCircle && <circle
+          r={newCircle.radius}
+          cx={newCircle.x}
+          cy={newCircle.y}
+        />}
       </svg>
     )
   }
