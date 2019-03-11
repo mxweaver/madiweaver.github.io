cb23593bdcd374d729319b1320f08946a68d3d28
Maya Vera <maya@mayavera.me>
2019-03-11 19:31:10 -0400

    created hoc for audio analyser

--- app/components/Spectrum/Spectrum.tsx
+++ app/components/Spectrum/Spectrum.tsx
@@ -1,50 +1,36 @@
 import React from 'react'
-import animate, { WrappedProps } from '../../hoc/animate';
+import { compose } from 'redux'
+import animate, { WrappedProps as AnimateProps } from '../../hoc/animate';
+import withAudioAnalyser, { WrappedProps as AudioProps } from '../../hoc/withAudioAnalyser';
 
-interface Props extends WrappedProps {
+interface Props extends AnimateProps, AudioProps {
   className?: string;
 }
 
 class Spectrum extends React.Component<Props> {
   private canvas = React.createRef<HTMLCanvasElement>()
   private ctx: CanvasRenderingContext2D
-  private audioContext?: AudioContext
-  private source?: MediaStreamAudioSourceNode
-  private analyser?: AnalyserNode
   private data?: Uint8Array
-  private x = 100
 
   public componentDidMount() {
     this.ctx = this.canvas.current.getContext('2d')
-
-    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
-      this.audioContext = new (window.AudioContext || window.webkitAudioContext)({
-        latencyHint: 'interactive'
-      })
-
-      this.source = this.audioContext.createMediaStreamSource(stream)
-
-      this.analyser = this.audioContext.createAnalyser()
-      this.analyser.fftSize = 1024
-
-      this.data = new Uint8Array(this.analyser.frequencyBinCount)
-      this.analyser.getByteFrequencyData(this.data)
-      this.source.connect(this.analyser)
-
-      this.props.onReady()
-    })
   }
 
   public componentDidUpdate(prevProps: Props) {
+    const { analyser, onReady } = this.props
+
+    if (analyser && !prevProps.analyser) {
+      this.data = new Uint8Array(analyser.frequencyBinCount)
+      onReady()
+    }
+
     if (this.props.step !== prevProps.step) {
       this.ctx.save();
       this.ctx.translate(1, 0);
       this.ctx.drawImage(this.canvas.current, 0, 0);
       this.ctx.restore();
+      analyser.getByteFrequencyData(this.data)
 
-      this.analyser.getByteFrequencyData(this.data)
-
-      this.x = (this.x + 20) % 256
       for (let i = 0; i < this.data.length; i++) {
         const v = this.data[i]
         this.ctx.fillStyle = `rgb(${v},${v},${v})`
@@ -58,4 +44,7 @@ class Spectrum extends React.Component<Props> {
   }
 }
 
-export default animate(60)(Spectrum)
+export default compose(
+  animate(60),
+  withAudioAnalyser(1024)
+)(Spectrum)
