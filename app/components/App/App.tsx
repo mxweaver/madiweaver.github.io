38ead4349f7b4958caf9ad540c065d94400b3ffc
Maya Vera <maya@mayavera.me>
2020-07-18 07:51:19 -0400

    added basic window style; fixed circle options position

--- app/components/App/App.tsx
+++ app/components/App/App.tsx
@@ -6,12 +6,14 @@ import c from './App.scss';
 
 const App: FC<{}> = () => (
   <HashRouter>
-    <div className={c.app}>
-      <Header />
-      <div className={c.content}>
-        {routes.games.map((route) => (
-          <Route exact key={route.url} path={route.url} component={route.component} />
-        ))}
+    <div className={c.container}>
+      <div className={c.app}>
+        <Header />
+        <div className={c.content}>
+          {routes.games.map((route) => (
+            <Route exact key={route.url} path={route.url} component={route.component} />
+          ))}
+        </div>
       </div>
     </div>
   </HashRouter>
